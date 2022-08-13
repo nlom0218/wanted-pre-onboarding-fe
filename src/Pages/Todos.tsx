7005322f92382337a3c54acef9e84421cc89dab8
nlom0218 <nlom0218@naver.com>
2022-08-13 23:50:06 +0900

    createTodo API

--- src/Pages/Todos.tsx
+++ src/Pages/Todos.tsx
@@ -1,7 +1,20 @@
 import * as React from "react";
+import createTodo from "../API/createTodo";
 
 const Todos = () => {
-  return <div>Todos</div>;
+  const create = async () => {
+    const data = await createTodo(
+      "공부하기",
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImhlbGxvQGhlbGxvLmNvbSIsImlhdCI6MTY2MDQwMjA2MCwiZXhwIjoxNjYxMDA2ODYwfQ.ttTB_p1fWFWJapcvA_kZCXSmmTRRbT4YIYV7dtt6eVI"
+    );
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <button onClick={create}>create todo</button>
+    </div>
+  );
 };
 
 export default Todos;
