4d0fbfc533c055f70c8f940ae156c7bda6c6233c
nlom0218 <nlom0218@naver.com>
2022-08-14 00:11:41 +0900

    deleteTodoAPI

--- src/Pages/Todos.tsx
+++ src/Pages/Todos.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import createTodo from "../API/createTodo";
 import getTodos from "../API/getTodos";
+import deleteTodoAPI from "../API/deleteTodoAPI";
 import updateTodoAPI from "../API/updateTodoAPI";
 
 const Todos = () => {
@@ -24,6 +25,13 @@ const Todos = () => {
     console.log(data);
   };
 
+  const deleteTodo = async () => {
+    await deleteTodoAPI(
+      455,
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImhlbGxkZGRvQGhlbGxvLmNvbSIsInN1YiI6MjU1LCJpYXQiOjE2NjA0MDI4ODQsImV4cCI6MTY2MTAwNzY4NH0.dkQigOXKmob1924an2QhZZRa14OMWkpnjCNgZ8o9f8o"
+    );
+  };
+
   React.useEffect(() => {
     const getTodo = async () => {
       const todos = await getTodos(
@@ -41,6 +49,7 @@ const Todos = () => {
     <div>
       <button onClick={create}>create todo</button>
       <button onClick={update}>update todo</button>
+      <button onClick={deleteTodo}>delete todo</button>
     </div>
   );
 };
