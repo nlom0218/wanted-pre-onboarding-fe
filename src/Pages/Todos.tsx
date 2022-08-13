51d2aeea09345000a3286a61538ebd2264d90d0b
nlom0218 <nlom0218@naver.com>
2022-08-14 00:08:34 +0900

    updateTodoAPI

--- src/Pages/Todos.tsx
+++ src/Pages/Todos.tsx
@@ -1,13 +1,25 @@
 import * as React from "react";
 import createTodo from "../API/createTodo";
 import getTodos from "../API/getTodos";
+import updateTodoAPI from "../API/updateTodoAPI";
 
 const Todos = () => {
   const [todos, setTodos] = React.useState();
+  console.log(todos);
   const create = async () => {
     const data = await createTodo(
       "공부하기",
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImhlbGxkZGRvQGhlbGxvLmNvbSIsInN1YiI6MjU1LCJpYXQiOjE2NjA0MDI4ODQsImV4cCI6MTY2MTAwNzY4NH0.dkQigOXKmob1924an2QhZZRa14OMWkpnjCNgZ8o9f8o"
+    );
+    console.log(data);
+  };
+
+  const update = async () => {
+    const data = await updateTodoAPI(
+      455,
+      "수정",
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImhlbGxkZGRvQGhlbGxvLmNvbSIsInN1YiI6MjU1LCJpYXQiOjE2NjA0MDI4ODQsImV4cCI6MTY2MTAwNzY4NH0.dkQigOXKmob1924an2QhZZRa14OMWkpnjCNgZ8o9f8o",
+      false
     );
     console.log(data);
   };
@@ -17,14 +29,18 @@ const Todos = () => {
       const todos = await getTodos(
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImhlbGxkZGRvQGhlbGxvLmNvbSIsInN1YiI6MjU1LCJpYXQiOjE2NjA0MDI4ODQsImV4cCI6MTY2MTAwNzY4NH0.dkQigOXKmob1924an2QhZZRa14OMWkpnjCNgZ8o9f8o"
       );
-      console.log(todos);
+      setTodos(todos);
     };
     getTodo();
+    return () => {
+      getTodo();
+    };
   }, []);
 
   return (
     <div>
       <button onClick={create}>create todo</button>
+      <button onClick={update}>update todo</button>
     </div>
   );
 };
