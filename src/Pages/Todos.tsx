07c86676703337628012a015f3ac645c8b0f4fce
nlom0218 <nlom0218@naver.com>
2022-08-16 21:04:46 +0900

    Create Todo Components

--- src/Pages/Todos.tsx
+++ src/Pages/Todos.tsx
@@ -3,6 +3,7 @@ import getTodosAPI from "../API/getTodosAPI";
 import { useNavigate } from "react-router-dom";
 import router from "../router";
 import Todo from "../Components/Todo";
+import CreateTodo from "../Components/CreateTodo";
 
 export interface ITodos {
   id: number;
@@ -12,39 +13,35 @@ export interface ITodos {
 }
 
 const Todos = () => {
+  const token = localStorage.getItem("token");
   const [todos, setTodos] = useState<ITodos[]>();
-  // const create = async () => {
-  //   const data = await createTodoAPI(
-  //     "공부하기2",
-  //     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im15ZW1haWxAZW1haWwuY29tIiwic3ViIjo2NjIsImlhdCI6MTY2MDY0MzMwMiwiZXhwIjoxNjYxMjQ4MTAyfQ.PBqC9JNL7bJNreqZ2IPv8v9I_XaVeZL8jGYwF_oAlps"
-  //   );
-  //   console.log(data);
-  // };
+  const navigate = useNavigate();
+
+  const getTodo = async () => {
+    if (!token) return;
+    const todos = await getTodosAPI(token);
+    setTodos(todos);
+  };
 
   useEffect(() => {
-    const getTodo = async () => {
-      const token = localStorage.getItem("token");
-      if (!token) return;
-      const todos = await getTodosAPI(token);
-      setTodos(todos);
-    };
     getTodo();
   }, []);
 
-  const navigate = useNavigate();
-
   useEffect(() => {
-    if (!Boolean(localStorage.getItem("token"))) {
+    if (!token) {
       navigate(router.signin);
     }
   }, []);
 
   return (
     <div>
-      {todos &&
-        todos.map((item) => {
-          return <Todo key={item.id} {...item} />;
-        })}
+      <CreateTodo token={token} getTodo={getTodo} />
+      <div>
+        {todos &&
+          todos.map((item) => {
+            return <Todo key={item.id} {...item} />;
+          })}
+      </div>
     </div>
   );
 };
