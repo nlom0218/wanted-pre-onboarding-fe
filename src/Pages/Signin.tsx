b6d98eb96b5b8e61e3db252cbd51712570521bf3
nlom0218 <nlom0218@naver.com>
2022-08-16 16:29:36 +0900

    Signin form basic layout

--- src/Pages/Signin.tsx
+++ src/Pages/Signin.tsx
@@ -2,11 +2,27 @@ import * as React from "react";
 import signinAPI from "../API/signinAPI";
 
 const Signin = () => {
-  const onClick = async () => {
-    const [token, error] = await signinAPI("hel@hedddllo.com", "heldfslohello");
-    console.log(token, error);
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const onSubmitForm = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const [token, error] = await signinAPI(email, password);
   };
-  return <div onClick={onClick}>Signin</div>;
+  return (
+    <form onSubmit={onSubmitForm}>
+      <input
+        type="text"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <input type="submit" value="Sign in" disabled={true} />
+    </form>
+  );
 };
 
 export default Signin;
