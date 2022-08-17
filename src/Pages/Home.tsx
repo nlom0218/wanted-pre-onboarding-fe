070fc0ed8210fd002e0a68c811f76057033edcb9
nlom0218 <nlom0218@naver.com>
2022-08-18 00:41:56 +0900

    onClickHome Btn

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,18 +1,23 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import router from "../router";
 
 const Container = styled.div`
+  width: 260px;
+  margin: 0 auto;
   display: flex;
   flex-direction: column;
   min-height: 100vh;
-  align-items: center;
   justify-content: center;
   div {
+    text-align: center;
     margin-bottom: 20px;
+    font-weight: 600;
   }
   button {
     box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
-    padding: 10px 60px;
+    padding: 10px;
     background-color: #e5a852;
     border-radius: 5px;
     border: none;
@@ -21,10 +26,14 @@ const Container = styled.div`
 `;
 
 const Home = () => {
+  const navigate = useNavigate();
+  const onClickBtn = () => {
+    navigate(router.signin);
+  };
   return (
     <Container>
-      <div>Wanted Pre Onboarding FE</div>
-      <button>Go to Sign In</button>
+      <div>Wanted Pre Onboarding FE 🚀</div>
+      <button onClick={onClickBtn}>Go to Sign In</button>
     </Container>
   );
 };
