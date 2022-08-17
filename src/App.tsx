6c5a1021ae6c26ad31a741d83f1a708fd69e0b98
nlom0218 <nlom0218@naver.com>
2022-08-18 00:33:44 +0900

    Home router

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import Signup from "./Pages/Signup";
 import Signin from "./Pages/Signin";
 import Todos from "./Pages/Todos";
 import router from "./router";
+import Home from "./Pages/Home";
 
 function App() {
   return (
     <Routes>
+      <Route path={router.home} element={<Home />} />
       <Route path={router.signin} element={<Signin />} />
       <Route path={router.signup} element={<Signup />} />
       <Route path={router.todos} element={<Todos />} />
--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,10 @@
 const SIGNIN = "/signin";
 const SIGNUP = "/signup";
 const TODOS = "/todos";
+const HOME = "/";
 
 const router = {
+  home: HOME,
   signin: SIGNIN,
   signup: SIGNUP,
   todos: TODOS,
